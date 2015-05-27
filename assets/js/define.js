b645032af0ed8ac3cad6e3d2695ac3d298952524
Thomas9693 <thomas9693@gmail.com>
2015-05-27 13:24:16 +0200

    Finished Question-Class

---
+++ assets/js/define.js
@@ -0,0 +1,17 @@
+// File
+
+var SERVER_URL = "http://wwwlehre.dhbw-stuttgart.de/~it14006/";
+var QUESTION_FILE = "q.json";
+
+// Konstanten
+
+var MAX_PLAYER_COUNT = 5;
+var ROUND_LIMIT = 10;
+var QUESTIONS_PER_ROUND = 15;
+
+var difficulty = {
+    low: 0,
+    intermediate: 1,
+    hard: 2,
+    extreme: 3
+};
