2a8863546ac2aa28becbab9de12a1b7dd19cf2a3
tobuto <tom.burmeister@ymail.com>
2015-07-11 23:37:52 +0200

    Mobile Anpassungen

--- assets/js/diamondscope.js
+++ assets/js/diamondscope.js
@@ -246,6 +246,8 @@ var DiamondScope = (function () {
     var widthBefore = -1;
     var prediff = -1;
     var intervalLength = 1/0;
+    var eventType = 'click';
+    var widthAdjustment = '';
     var interval = setInterval(function(){ 
         content = '';
         if(intervalLength-- < 1) {
@@ -257,14 +259,29 @@ var DiamondScope = (function () {
     }, 1000);
 
     var init = function () {
-        eventhandler();
         $.getJSON(SERVER_URL + QUESTION_FILE, function (data) {
+            
             questionArray = data;
             questionArray.forEach(function (e, i) {
                 questionArray[i] = new Question(e.question, e.answers, e.rightAnswer, e.difficulty, e.id);
             });
-            music_enabled = true;
-            initializeGame();
+            
+            if(checkForMobile()){
+                eventType = 'tap';
+                $.ajax({
+                    url      : './assets/js/jquery.mobile.custom.min.js',
+                    dataType : 'script',
+                    success : function () {
+                        initializeGame();
+                        
+                        $(window).on('orientationchange', function(){
+                            verticalAlign();
+                        });
+                    }
+                });
+            } else {
+                initializeGame();
+            }
         });
     };
 
@@ -274,7 +291,7 @@ var DiamondScope = (function () {
             sizeCheck();
         });
 
-        $(document).on('click', '#joker-fifty', function () {
+        $(document).on(eventType, '#joker-fifty', function () {
 
             $(this).addClass('disabled');
 
@@ -289,12 +306,12 @@ var DiamondScope = (function () {
 
         });
 
-        $(document).on('click', '#joker-audience', function () {
+        $(document).on(eventType, '#joker-audience', function () {
 
             $(this).addClass('disabled');
-
+            
             answers = game.players[game.currentPlayer].useJoker(1);
-            if (answers != 0) $('#audience').html('.audience:after{opacity:1;}.audience{color:transparent;}');
+            if (answers != 0) $('#audience').html('.audience:after{opacity:1;' + widthAdjustment + '}.audience{color:transparent;}');
             else answers = [];
 
             answers.forEach(function (e, i) {
@@ -304,7 +321,7 @@ var DiamondScope = (function () {
 
         });
 
-        $(document).on('click', '#sound-button', function () {
+        $(document).on(eventType, '#sound-button', function () {
             if (music_enabled) {
                 $(this).addClass('off');
                 $('audio').each(function () {
@@ -321,7 +338,7 @@ var DiamondScope = (function () {
             }
         })
 
-        $(document).on('click', '.answer', function () {
+        $(document).on(eventType, '.answer', function () {
             if (!($(this).hasClass('disabled'))) {
 
                 $('.answer').addClass('disabled');
@@ -408,6 +425,7 @@ var DiamondScope = (function () {
         }
 
         if ($(window).width() < 1200 && widthBefore >= 1200) {
+            widthAdjustment = 'width: 15%;';
             $('body > .container-fluid').css('height', '0');
             $('body > #main-center.container-fluid').css({
                 'height': '99%'
@@ -422,6 +440,7 @@ var DiamondScope = (function () {
             $('body, *').css('font-weight', 300);
 
         } else if ($(window).width() >= 1200 && widthBefore < 1200) {
+            widthAdjustment = '';
             $('body > .container-fluid').css('height', '15%');
             $('body > #main-center.container-fluid').css({
                 'height': '70%'
@@ -478,6 +497,8 @@ var DiamondScope = (function () {
     };
 
     var initializeGame = function () {
+        eventhandler();
+        music_enabled = true;
         game = new Game(questionArray);
         draw.startMenu();
     };
@@ -553,13 +574,15 @@ var DiamondScope = (function () {
 
             $('#content-div').html(content);
 
-            $('#singleplayer-button').on('click', function () {
-                $(this).off('click');
+            if(!music_enabled) $('#sound-button').addClass('off');
+            
+            $('#singleplayer-button').on(eventType, function () {
+                $(this).off(eventType);
                 changeScreen(singlePlayer);
             });
 
-            $('#multiplayer-button').on('click', function () {
-                $(this).off('click');
+            $('#multiplayer-button').on(eventType, function () {
+                $(this).off(eventType);
                 changeScreen(multiPlayer);
             });
 
@@ -610,13 +633,13 @@ var DiamondScope = (function () {
 
             $('#content-div').html(content);
 
-            $('#back-button').on('click', function () {
-                $(this).off('click');
+            $('#back-button').on(eventType, function () {
+                $(this).off(eventType);
                 changeScreen(startMenu);
             });
 
-            $('#next-button').on('click', function () {
-                $(this).off('click');
+            $('#next-button').on(eventType, function () {
+                $(this).off(eventType);
                 game.addPlayer($('#player-name').val());
                 game.gameMode = 0; // 0 -> Singleplayer
                 changeScreen(questionScreen);
@@ -667,12 +690,12 @@ var DiamondScope = (function () {
 
             $('#content-div').html(content);
 
-            $('#back-button').on('click', function () {
-                $(this).off('click');
+            $('#back-button').on(eventType, function () {
+                $(this).off(eventType);
                 changeScreen(startMenu);
             });
 
-            $('.count').on('click', function () {
+            $('.count').on(eventType, function () {
 
                 $('.count').removeClass('selected');
                 $(this).addClass('selected');
@@ -689,7 +712,7 @@ var DiamondScope = (function () {
                 sizeCheck();
             });
 
-            $('#next-button').on('click', function () {
+            $('#next-button').on(eventType, function () {
 
                 $('.names').each(function () {
                     game.addPlayer($(this).val());
@@ -798,7 +821,7 @@ var DiamondScope = (function () {
 
             $('#content-div').html(content);
 
-            $('#new-game-button').on('click', function () {
+            $('#new-game-button').on(eventType, function () {
 
                 if (game.round++ < ROUND_LIMIT) {
 
@@ -815,13 +838,13 @@ var DiamondScope = (function () {
                 } else {
 
                     $(this).html('<h1>Es ist leider nicht möglich mehr als ' + ROUND_LIMIT + ' Runden zu spielen</h1>');
-                    $(this).off('click');
+                    $(this).off(eventType);
 
                 }
 
             });
 
-            $('#report-button').on('click', function () {
+            $('#report-button').on(eventType, function () {
                 reportScreen();
             });
 
@@ -854,7 +877,7 @@ var DiamondScope = (function () {
 
             $('#content-div').html(content);
 
-            $('#menu-button').on('click', function () {
+            $('#menu-button').on(eventType, function () {
                 game = new Game(questionArray);
                 startMenu();
             });
@@ -1052,6 +1075,12 @@ var DiamondScope = (function () {
         })(questionArray.length - 1);
     };
 
+    function checkForMobile () {
+        var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;   
+    }
+    
     return {
         init: init,
         sizeCheck: sizeCheck,
